3b7836ce7bcff61f0519c53ab3439ccb628329ef
Rasha Obaid <rasha-rashad1997@hotmail.com>
2020-07-27 09:50:43 +0300

    update in top header

--- assets/js/slickmain.js
+++ assets/js/slickmain.js
@@ -1,32 +1,37 @@
 $(document).ready(function () {
 
     $('.Slickslider').slick({
-        infinite: false,
+        
+        infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
-        arrows: true,
+        arrows: false,
+        speed:300,
         dots: false,
-        // rtl: true,
+        autoplay:true,
+        autoplaySpeed:1700,
         responsive: [{
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 1,
-                    // infinite: true,
+                    slidesToScroll: 2,
+                    infinite: true,
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1
+                    slidesToScroll: 2,
+                    infinite: true,
                 }
             },
             {
                 breakpoint: 400,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    infinite: true,
                 }
             }
 
